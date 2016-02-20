d07095248b97a143ea54d9338b8fb5a1500180c1
Ryan Hayle <rhayle@arcstone.com>
2016-02-19 18:39:21 -0600

    Use explicit annotation to inject $scope into controller.

--- addtocalendar.js
+++ addtocalendar.js
@@ -1,143 +1,166 @@
 'use strict';
 
 angular.module('jshor.angular-addtocalendar', [])
-	.controller('AddtocalendarCtrl', function($scope) {
+    .controller('AddtocalendarCtrl', [
+        '$scope', function ($scope) {
+            var cal = ics();
 
-		var cal = ics();
+            var utcToDate = function (date) {
+                var dateObj = new Date();
 
-		var utcToDate = function(date) {
-			var dateObj = new Date();
-			dateObj.setFullYear(date.substring(0, 4));
-			dateObj.setDate(date.substring(6, 8));
-			dateObj.setMonth(parseInt(date.substring(4, 6))-1);
-			dateObj.setUTCHours(date.substring(9, 11));
-			dateObj.setUTCMinutes(date.substring(11, 13));
-			dateObj.setUTCSeconds(date.substring(13, 15));
-			return dateObj.toString();
-		};
+                dateObj.setFullYear(date.substring(0, 4));
+                dateObj.setDate(date.substring(6, 8));
+                dateObj.setMonth(parseInt(date.substring(4, 6)) - 1);
+                dateObj.setUTCHours(date.substring(9, 11));
+                dateObj.setUTCMinutes(date.substring(11, 13));
+                dateObj.setUTCSeconds(date.substring(13, 15));
 
-		var toDate = function(date) {
-			var dateObj = new Date();
-			dateObj.setFullYear(date.substring(0, 4));
-			dateObj.setDate(date.substring(6, 8));
-			dateObj.setMonth(parseInt(date.substring(4, 6))-1);
-			dateObj.setHours(date.substring(9, 11));
-			dateObj.setMinutes(date.substring(11, 13));
-			dateObj.setSeconds(date.substring(13, 15));
-			return dateObj.toString();
-		};
+                return dateObj.toString();
+            };
 
-		$scope.description = $scope.description || '';
-		$scope.getIcsCalendarUrl = function() {
-			cal.addEvent($scope.title, $scope.description, $scope.location, ($scope.utc == true) ? utcToDate($scope.startDate) : toDate($scope.startDate), ($scope.utc == true) ? utcToDate($scope.endDate) : toDate($scope.endDate));
-			return cal.download();
-		};
+            var toDate = function (date) {
+                var dateObj = new Date();
 
-		var getYahooCalendarUrl = function() {
-			var yahooCalendarUrl = 'http://calendar.yahoo.com/?v=60&view=d&type=20';
-			yahooCalendarUrl += '&title=' + encodeURI($scope.title);
-			yahooCalendarUrl += '&st=' + encodeURI($scope.startDate) + '&et=' + encodeURI($scope.endDate);
-			yahooCalendarUrl += '&desc=' + encodeURI($scope.description);
-			yahooCalendarUrl += '&in_loc=' + encodeURI($scope.location);
+                dateObj.setFullYear(date.substring(0, 4));
+                dateObj.setDate(date.substring(6, 8));
+                dateObj.setMonth(parseInt(date.substring(4, 6)) - 1);
+                dateObj.setHours(date.substring(9, 11));
+                dateObj.setMinutes(date.substring(11, 13));
+                dateObj.setSeconds(date.substring(13, 15));
 
-			return yahooCalendarUrl;
-		};
+                return dateObj.toString();
+            };
 
-		var getGoogleCalendarUrl = function() {
-			var googleCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
-			googleCalendarUrl += '&text=' + encodeURI($scope.title);
-			googleCalendarUrl += '&dates=' + encodeURI($scope.startDate) + '/' + encodeURI($scope.endDate);
-			googleCalendarUrl += '&details=' + encodeURI($scope.description);
-			googleCalendarUrl += '&location=' + encodeURI($scope.location);
+            $scope.description = $scope.description || '';
 
-			return googleCalendarUrl;
-		};
+            $scope.getIcsCalendarUrl = function () {
+                cal.addEvent(
+                    $scope.title,
+                    $scope.description,
+                    $scope.location,
+                    ($scope.utc == true) ? utcToDate($scope.startDate) : toDate($scope.startDate),
+                    ($scope.utc == true) ? utcToDate($scope.endDate) : toDate($scope.endDate)
+                );
 
-		var getMicrosoftCalendarUrl = function() {
-			var microsoftCalendarUrl = 'http://calendar.live.com/calendar/calendar.aspx?rru=addevent';
-			microsoftCalendarUrl += '&summary=' + encodeURI($scope.title);
-			microsoftCalendarUrl += '&dtstart=' + encodeURI($scope.startDate) + '&dtend=' + encodeURI($scope.endDate);
-			microsoftCalendarUrl += '&description=' + encodeURI($scope.description);
-			microsoftCalendarUrl += '&location=' + encodeURI($scope.location);
+                return cal.download();
+            };
 
-			return microsoftCalendarUrl;
-		};
+            var getYahooCalendarUrl = function () {
+                var yahooCalendarUrl = 'http://calendar.yahoo.com/?v=60&view=d&type=20';
 
-		$scope.calendarUrl = {
-			microsoft : getMicrosoftCalendarUrl(),
-			google 		: getGoogleCalendarUrl(),
-			yahoo 		: getYahooCalendarUrl()
-		};
+                yahooCalendarUrl += '&title=' + encodeURI($scope.title);
+                yahooCalendarUrl += '&st=' + encodeURI($scope.startDate) + '&et=' + encodeURI($scope.endDate);
+                yahooCalendarUrl += '&desc=' + encodeURI($scope.description);
+                yahooCalendarUrl += '&in_loc=' + encodeURI($scope.location);
 
-		$scope.calendarTypes = [
-			{
-				"text":"iCalendar",
-				"click":"getIcsCalendarUrl()"
-			},
-			{
-				"text":"Google Calendar",
-				"href":$scope.calendarUrl.google,
-				"target":"_blank"
-			},
-			{
-				"text":"Outlook",
-				"click":"getIcsCalendarUrl()"
-			},
-			{
-				"text":"Yahoo! Calendar",
-				"href":$scope.calendarUrl.yahoo,
-				"target":"_blank"
-			},
-			{
-				"text":"Microsoft Calendar",
-				"href":$scope.calendarUrl.microsoft,
-				"target":"_blank"
-			}
-		];
-	})
-	.directive('addtocalendar', function() {
-    return {
-      restrict: 'E',
-      scope: {
-        startDate 	: '@',
-        endDate 	: '@',
-        title 		: '@',
-        description 	: '@',
-        location 	: '@',
-        className 	: '@',
-        btnText 	: '@'
-      },
-    	controller: 'AddtocalendarCtrl',
-      template: '\
-				<div class="btn-group" dropdown on-toggle="toggled(open)">\
-					<span ng-class="className || \'btn btn-sm btn-default dropdown-toggle\'" dropdown-toggle>\
-					{{btnText || \'Add to calendar\'}} <span class="caret"></span>\
-					</span>\
-				  <ul class="dropdown-menu">\
-				    <li><a href="#" ng-click="getIcsCalendarUrl()">iCalendar</a></li>\
-				    <li><a href="{{calendarUrl.google}}" target="_blank">Google Calendar</a></li>\
-				    <li><a href="#" ng-click="getIcsCalendarUrl()">Outlook</a></li>\
-				    <li><a href="{{calendarUrl.yahoo}}" target="_blank">Yahoo! Calendar</a></li>\
-				    <li><a href="{{calendarUrl.microsoft}}" target="_blank">Microsoft Calendar</a></li>\
-				  </ul>\
-				</div>\
-			'
-		};
-	}).directive('addtocalendarstrap', function(){
-		return {
-			restrict: 'E',
-			scope: {
-				startDate 	: '@',
-				endDate 	: '@',
-				title 		: '@',
-				description 	: '@',
-				location 	: '@',
-				className 	: '@',
-				btnText 	: '@'
-			},
-			controller: 'AddtocalendarCtrl',
-			template: '\
-				<button type="button" class="btn btn-sm btn-default" data-animation="am-flip-x" bs-dropdown="calendarTypes" aria-haspopup="true" aria-expanded="false">Add to calendar</button> \
-				'
-		};
-	});
+                return yahooCalendarUrl;
+            };
+
+            var getGoogleCalendarUrl = function () {
+                var googleCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
+
+                googleCalendarUrl += '&text=' + encodeURI($scope.title);
+                googleCalendarUrl += '&dates=' + encodeURI($scope.startDate) + '/' + encodeURI($scope.endDate);
+                googleCalendarUrl += '&details=' + encodeURI($scope.description);
+                googleCalendarUrl += '&location=' + encodeURI($scope.location);
+
+                return googleCalendarUrl;
+            };
+
+            var getMicrosoftCalendarUrl = function () {
+                var microsoftCalendarUrl = 'http://calendar.live.com/calendar/calendar.aspx?rru=addevent';
+
+                microsoftCalendarUrl += '&summary=' + encodeURI($scope.title);
+                microsoftCalendarUrl += '&dtstart=' + encodeURI($scope.startDate) + '&dtend=' +
+                                        encodeURI($scope.endDate);
+                microsoftCalendarUrl += '&description=' + encodeURI($scope.description);
+                microsoftCalendarUrl += '&location=' + encodeURI($scope.location);
+
+                return microsoftCalendarUrl;
+            };
+
+            $scope.calendarUrl = {
+                microsoft: getMicrosoftCalendarUrl(),
+                google:    getGoogleCalendarUrl(),
+                yahoo:     getYahooCalendarUrl()
+            };
+
+            $scope.calendarTypes = [
+                {
+                    "text":  "iCalendar",
+                    "click": "getIcsCalendarUrl()"
+                },
+                {
+                    "text":   "Google Calendar",
+                    "href":   $scope.calendarUrl.google,
+                    "target": "_blank"
+                },
+                {
+                    "text":  "Outlook",
+                    "click": "getIcsCalendarUrl()"
+                },
+                {
+                    "text":   "Yahoo! Calendar",
+                    "href":   $scope.calendarUrl.yahoo,
+                    "target": "_blank"
+                },
+                {
+                    "text":   "Microsoft Calendar",
+                    "href":   $scope.calendarUrl.microsoft,
+                    "target": "_blank"
+                }
+            ];
+        }
+    ])
+    .directive('addtocalendar', function () {
+        return {
+            restrict:   'E',
+            scope:      {
+                startDate:   '@',
+                endDate:     '@',
+                title:       '@',
+                description: '@',
+                location:    '@',
+                className:   '@',
+                btnText:     '@'
+            },
+            controller: 'AddtocalendarCtrl',
+            template:   '\
+            <div class="btn-group" dropdown on-toggle="toggled(open)">\
+                <span ng-class="className || \'btn btn-sm btn-default dropdown-toggle\'" dropdown-toggle>\
+                {{btnText || \'Add to calendar\'}} <span class="caret"></span>\
+                </span>\
+              <ul class="dropdown-menu">\
+                <li><a href="#" ng-click="getIcsCalendarUrl()">iCalendar</a></li>\
+                <li><a href="{{calendarUrl.google}}" target="_blank">Google Calendar</a></li>\
+                <li><a href="#" ng-click="getIcsCalendarUrl()">Outlook</a></li>\
+                <li><a href="{{calendarUrl.yahoo}}" target="_blank">Yahoo! Calendar</a></li>\
+                <li><a href="{{calendarUrl.microsoft}}" target="_blank">Microsoft Calendar</a></li>\
+              </ul>\
+            </div>\
+        '
+        };
+    })
+    .directive('addtocalendarstrap', function () {
+        return {
+            restrict:   'E',
+            scope:      {
+                startDate:   '@',
+                endDate:     '@',
+                title:       '@',
+                description: '@',
+                location:    '@',
+                className:   '@',
+                btnText:     '@'
+            },
+            controller: 'AddtocalendarCtrl',
+            template:   '\
+            <button type="button" \
+                    class="btn btn-sm btn-default" \
+                    data-animation="am-flip-x" \
+                    bs-dropdown="calendarTypes" \
+                    aria-haspopup="true" \
+                    aria-expanded="false">Add to calendar</button>\
+            '
+        };
+    });
